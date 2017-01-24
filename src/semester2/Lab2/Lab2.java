d4d5d2c70ccd684e9913e0ede2e9a43e4096c903
Mansi <Mansi@10.250.6.185>
2017-01-24 17:06:29 -0500

    Lab2

---
+++ src/semester2/Lab2/Lab2.java
@@ -0,0 +1,13 @@
+
+package semester2.Lab2;
+
+/**
+ *
+ * @author Mansi
+ */
+public class Lab2 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World, my GitHub account name is MansiPatel23");
+    }
+}
